f1c63e0f4540a67d62046c8cdaede89c1895e837
NelsonOrtiz94 <ortnn990@gmail.com>
2024-11-29 19:38:23 -0500

    Se implementan los hooks: - useEffect - useState

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    console.log("efecct", { enabled });
+  }, [enabled]);
+
   return (
-    <h3>Proyecto #3</h3>
-  )
+    <>
+      <h3>Proyecto #3</h3>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} puntero
+      </button>
+    </>
+  );
 }
 
-export default App
+export default App;
