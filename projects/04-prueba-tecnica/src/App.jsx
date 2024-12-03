1d3755eb4b5b0f9b5dd49693b003dda9aaebf6ea
NelsonOrtiz94 <ortnn990@gmail.com>
2024-12-03 10:53:57 -0500

    Se agregan cambios en cuanto a estrcutura

--- projects/04-prueba-tecnica/src/App.jsx
+++ projects/04-prueba-tecnica/src/App.jsx
@@ -1,24 +1,33 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
-export function App () {
-    const [fact, setFact] = useState()
+export function App() {
+  const [fact, setFact] = useState();
 
-    useEffect(() => {
-        async function getRandomFact() {
-            const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
-            const json = await res.json()
-            setFact(json.fact)
-        }
-        getRandomFact()
-    }, [])
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then((response) => response.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
 
-    return (
-        <div>
-        <h1>App de gatos</h1>
-        {fact && <p>{fact}</p> }
-        </div>
-    )
+        const firstWord = fact.split(" ", 3);
+        console.log(firstWord);
+
+        fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+            .then((res) => res.json())
+            .then((response) => {
+                console.log(response);
+            });
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Prueba App</h1>
+      {fact && <p>{fact}</p>}
+    </div>
+  );
 }
