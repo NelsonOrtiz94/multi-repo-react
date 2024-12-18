cc1f72b338621db6d4155e23715c3adc8279a4d2
NelsonOrtiz94 <ortnn990@gmail.com>
2024-12-17 20:10:47 -0500

    Se obtienen datos de la película como Id, title, img, year y se procede a visualizar en la interfaz

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import responseMovies from "./mocks/results.json";
+import noResults from "./mocks/no-results.json";
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies && movies.length > 0;
   return (
     <div className="page">
-
       <header>
-        <h1>Movie <span className="text-warning">search</span></h1>
+        <h1>
+          Movie <span className="text-warning">search</span>
+        </h1>
         <form className="form">
           <input placeholder="Movie name" />
           <button type="submit">Search</button>
@@ -13,9 +18,20 @@ function App() {
       </header>
 
       <main>
-        Search results
+        {hasMovies ? (
+          <ul className="movies">
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <h2>{movie.Title}</h2>
+                <img src={movie.Poster} alt={movie.Title} />
+                <p>{movie.Year}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No movies found</p>
+        )}
       </main>
-
     </div>
   );
 }
