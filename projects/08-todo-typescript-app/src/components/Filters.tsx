8d1dd49150560ee676b9db9b195f03bd589e6d77
NelsonOrtiz94 <ortnn990@gmail.com>
2025-01-15 20:43:51 -0500

    Se estructuran hooks para los casos todo

--- projects/08-todo-typescript-app/src/components/Filters.tsx
+++ projects/08-todo-typescript-app/src/components/Filters.tsx
@@ -19,22 +19,22 @@ export const Filters: React.FC<Props> = ({ filterSelected, handleFilterChange })
   }
 
   return (
-  <ul className="filters">
-    {
-      Object.entries(FILTERS_BUTTONS).map(([key, { href, literal }]) => {
-        const isSelected = key === filterSelected
-        const className = isSelected ? 'selected' : ''
+    <ul className="filters">
+      {
+        Object.entries(FILTERS_BUTTONS).map(([key, { href, literal }]) => {
+          const isSelected = key === filterSelected
+          const className = isSelected ? 'selected' : ''
 
-        return (
-          <li key={key}>
-            <a href={href}
-              className={className}
-              onClick={handleClick(key as FilterValue)}>{literal}
-            </a>
-          </li>
-        )
-      })
-    }
-  </ul>
+          return (
+            <li key={key}>
+              <a href={href}
+                className={className}
+                onClick={handleClick(key as FilterValue)}>{literal}
+              </a>
+            </li>
+          )
+        })
+      }
+    </ul>
   )
 }
