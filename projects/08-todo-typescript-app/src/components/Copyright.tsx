26199565ed97000e960dcd761f8798a108538541
NelsonOrtiz94 <ortnn990@gmail.com>
2025-01-17 15:19:29 -0500

    Se dá por concluida la app para todos.

--- projects/08-todo-typescript-app/src/components/Copyright.tsx
+++ projects/08-todo-typescript-app/src/components/Copyright.tsx
@@ -2,7 +2,7 @@ import './Copyright.css'
 
 export const Copyright: React.FC = () => (
   <footer className='copyright'>
-    <h4>Curso de React desde cero ⚛️ － <span>@midudev</span></h4>
+    <h4>Proyecto Todo ⚛️ － <span>Nelson Ortiz</span></h4>
     <h5>Creando un TODO con TypeScript</h5>
   </footer>
 )
