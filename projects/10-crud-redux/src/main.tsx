738ec9a1322ad85bb0f6f1f26016007d4e44cce1
NelsonOrtiz94 <ortnn990@gmail.com>
2025-02-14 16:31:16 -0500

    Se anexan faltantes para llevar a cabo estructura de la app

--- projects/10-crud-redux/src/main.tsx
+++ projects/10-crud-redux/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+);
